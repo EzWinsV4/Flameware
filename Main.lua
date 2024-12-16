2ca19ee6a4972659e8da1d54ae45b252b6eefad2
EzWinsV4 <vvv.playz16@gmail.com>
2024-12-16 17:47:01 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,15 @@
--- soon
+local function runscript()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Aimlock.lua", true))()
+  wait(0.3)
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/ESP.lua", true))()
+  wait(0.3)
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/ProjectileExploit.lua", true))()
+  wait(0.3)
+end
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "Flame 2.0", 
+    Text = "Flame for Rivals Script Executed! // Q to lock, E for ESP!", 
+    Duration = 4,
+    Button1  = "Cool",
+})
