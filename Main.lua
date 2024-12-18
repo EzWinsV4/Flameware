7aabf7c142801b5bbed476da1c55da46e5392411
EzWinsV4 <vvv.playz16@gmail.com>
2024-12-18 18:46:36 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,14 +1,16 @@
 local FlameExecuted = false
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Aimlock.lua", true))()
-wait(0.5)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/ESP.lua", true))()
-wait(0.5)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Speed.lua", true))()
-wait(0.5)
-
 if not FlameExecuted then
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Aimlock.lua", true))()
+    wait(0.5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/ESP.lua", true))()
+    wait(0.5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Speed.lua", true))()
+    wait(0.5)
+    
     FlameExecuted = true
+    
     game:GetService("StarterGui"):SetCore("SendNotification",{
         Title = "Flame 2.0", 
         Text = "Flame for Rivals Script Executed! // Q to lock, E for ESP!", 
