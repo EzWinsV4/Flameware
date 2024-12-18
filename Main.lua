1a11dce7939b67ce6cd7e4e9fa58d7e528429870
EzWinsV4 <vvv.playz16@gmail.com>
2024-12-18 18:54:50 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,6 @@
-local FlameExecuted = false
+getgenv().FlameExecuted = getgenv().FlameExecuted or false
 
-if not FlameExecuted then
-
+if not getgenv().FlameExecuted then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Aimlock.lua", true))()
     wait(0.5)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/ESP.lua", true))()
@@ -9,17 +8,17 @@ if not FlameExecuted then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Speed.lua", true))()
     wait(0.5)
     
-    FlameExecuted = true
+    getgenv().FlameExecuted = true
     
-    game:GetService("StarterGui"):SetCore("SendNotification",{
+    game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "Flame 2.0", 
-        Text = "Flame for Rivals Script Executed! // Q to lock, E for ESP!", 
+        Text = "Flame for Rivals Script Executed!", 
         Duration = 4, 
         Button1 = "Cool"
     })
 else
-    game:GetService("StarterGui"):SetCore("SendNotification",{
-        Title = "Double Script Execute", 
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "!Error:", 
         Text = "Script has already been executed!", 
         Duration = 4
     })
