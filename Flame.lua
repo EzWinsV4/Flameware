9fd7b60b8d5b54b8f9991c1f82378f0be670413a
EzWinsV4 <vvv.playz16@gmail.com>
2024-12-22 00:47:30 +0000

    Update Flame.lua

--- Flame.lua
+++ Flame.lua
@@ -7,22 +7,30 @@ local FlameOwner = {
 local FlamePrivateUsers = {
     2758145996,
 }
-local FlameUsers = {}
 
-if table.find(FlameOwner, player.UserId) then
-  game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Flame OWNER 👑", 
-    Text = "Hello Flame owner >:) ",
-    Duration = 4,
-    Button1 = "Wsg",
-  })
-end
+local isOwner = table.find(FlameOwner, LocalPlayer.UserId) ~= nil
+local isPrivateUser = table.find(FlamePrivateUsers, LocalPlayer.UserId) ~= nil
+local FlameUser = not isOwner and not isPrivateUser
 
-if table.find(FlamePrivateUsers, player.UserId) then
-  game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Flame Private 💎", 
-    Text = "Hello Flame Private user!",
-    Duration = 4,
-    Button1 = "HELLO",
-  })
+if isOwner then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Flame OWNER 👑", 
+        Text = "Hello Flame owner >:) ",
+        Duration = 4,
+        Button1 = "Wsg",
+    })
+elseif isPrivateUser then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Flame Private 💎", 
+        Text = "Hello Flame Private user!",
+        Duration = 4,
+        Button1 = "HELLO",
+    })
+elseif isFlameUser then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Flame User 🔥", 
+        Text = "Welcome to Flame!",
+        Duration = 4,
+        Button1 = "Okay",
+    })
 end
