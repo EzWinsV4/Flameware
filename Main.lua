3b57bc5018eefea4636bdcb936e77c18c02ae74e
EzWinsV4 <vvv.playz16@gmail.com>
2024-12-25 15:24:06 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -12,6 +12,8 @@ if not getgenv().FlameExecuted then
     task.wait(0.2)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Speed.lua", true))()
     task.wait(0.2)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Noclip.lua", true))()
+    task.wait(0.2)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Teleport.lua", true))()
     task.wait(0.2)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/TpFollow.lua", true))()
