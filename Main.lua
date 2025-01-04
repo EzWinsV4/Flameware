dc448876b028d634d508f2d2bd01a3963a826d94
EzWinsV4 <vvv.playz16@gmail.com>
2025-01-04 22:05:48 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -19,8 +19,10 @@ if not getgenv().FlameExecuted then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/TpFollow.lua", true))()
     task.wait(0.1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/Flight.lua", true))()
-    --task.wait(0.1)
-    --loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/Antivoid.lua", true))()
+    task.wait(0.1)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/Antivoid.lua", true))()
+    task.wait(0.1)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/ProjectileExploit.lua", true))()
     task.wait(0.2)
 
     game:GetService("StarterGui"):SetCore("SendNotification", {
