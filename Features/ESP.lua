40b75333c545746ba75976cb99b733d2401c6830
EzWinsV4 <vvv.playz16@gmail.com>
2025-01-06 21:28:18 +0000

    Update ESP.lua

--- Features/ESP.lua
+++ Features/ESP.lua
@@ -1,33 +1,19 @@
 local Players = game:GetService("Players")
 local UserInputService = game:GetService("UserInputService")
-
 local ESP = false
 
 local function EspActivate(player)
     if player.Character then
         local existingEsp = player.Character:FindFirstChild("Highlight")
-
         if not ESP then
-            if existingEsp then
-                existingEsp:Destroy()
-            end
+            if existingEsp then existingEsp:Destroy() end
             return
         end
-
-        if existingEsp then
-            existingEsp:Destroy()
-        end
-
+        if existingEsp then existingEsp:Destroy() end
         local highlight = Instance.new("Highlight")
         highlight.Parent = player.Character
         highlight.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-
-        if player.Team == Players.LocalPlayer.Team then
-            highlight.FillColor = Color3.new(0, 1, 0)
-        else
-            highlight.FillColor = Color3.new(1, 0, 0)
-        end
-
+        highlight.FillColor = (player.Team == Players.LocalPlayer.Team) and Color3.new(0, 1, 0) or Color3.new(1, 0, 0)
         highlight.OutlineColor = Color3.new(0, 0, 0)
     end
 end
@@ -35,38 +21,28 @@ end
 local function toggleEsp()
     ESP = not ESP
     for _, player in pairs(Players:GetPlayers()) do
-        if player.Character then
-            EspActivate(player)
-        end
+        if player.Character then EspActivate(player) end
     end
 end
 
 for _, player in pairs(Players:GetPlayers()) do
-    if player.Character then
-        EspActivate(player)
-    end
-
-    player.CharacterAdded:Connect(function()
-        EspActivate(player)
-    end)
+    if player.Character then EspActivate(player) end
+    player.CharacterAdded:Connect(function() EspActivate(player) end)
+    player.TeamChanged:Connect(function() EspActivate(player) end)
 end
 
 Players.PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Connect(function()
-        EspActivate(player)
-    end)
+    player.CharacterAdded:Connect(function() EspActivate(player) end)
+    player.TeamChanged:Connect(function() EspActivate(player) end)
 end)
 
 UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
     if gameProcessedEvent then return end
-
-    if input.KeyCode == Enum.KeyCode.E then
-        toggleEsp()
-    end
+    if input.KeyCode == Enum.KeyCode.E then toggleEsp() end
 end)
 
-game:GetService("StarterGui"):SetCore("SendNotification",{
+game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Flame", 
     Text = "ESP Loaded! // E for ESP!", 
-    Duration = 2,
+    Duration = 2
 })
