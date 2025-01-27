5fd5ed60dc493ce9a1b76b50f6ddfe3b22a167a0
EzWinsV4 <vvv.playz16@gmail.com>
2025-01-27 13:05:29 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -20,7 +20,7 @@ if not getgenv().FlameExecuted then
     task.wait(0.1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/Flight.lua", true))()
     task.wait(0.1)
---    loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/Antivoid.lua", true))()
+--    loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/Blacklist.lua", true))()
 --    task.wait(0.1)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Features/HitboxExpander.lua", true))()
     task.wait(0.2)
