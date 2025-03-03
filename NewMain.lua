2af8ed506fbf7482a2a2211841efbb1f3486558a
EzWinsV4 <vvv.playz16@gmail.com>
2025-03-03 20:02:05 +0000

    Update NewMain.lua

--- NewMain.lua
+++ NewMain.lua
@@ -1,2 +1,2 @@
 task.wait(0.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Flameware/script.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Flameware/rivals.lua", true))()
