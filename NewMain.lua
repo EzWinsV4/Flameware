c1943409cca6a3dc74186c6903f936cb9d2e3d63
EzWinsV4 <vvv.playz16@gmail.com>
2025-03-04 19:29:27 +0000

    Update NewMain.lua

--- NewMain.lua
+++ NewMain.lua
@@ -1,14 +1,2 @@
-if not getgenv().FlamewareExecuted then
-
-    if not game:IsLoaded() then
-        game.Loaded:Wait()
-    end
-    
-    getgenv().FlamewareExecuted = true
-
-    task.wait(0.1)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Flameware/rivals.lua", true))()
-  
-else
-  print(" |❌|: FLAMEWARE IS ALREADY EXECUTED :|❌| ")
-end
+task.wait(0.1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EzWinsV4/FlameForRobloxRivals/refs/heads/main/Flameware/rivals.lua", true))()
